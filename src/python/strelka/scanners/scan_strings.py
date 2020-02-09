2f3091432674111951142653c175686ea0ce7a4c
ulopong <ulopong@byteprose.com>
2020-02-09 17:02:15 -0600

    add flarestrings capability

--- src/python/strelka/scanners/scan_strings.py
+++ src/python/strelka/scanners/scan_strings.py
@@ -1,4 +1,10 @@
 import re
+import io
+import os
+import subprocess
+import sys
+import shlex
+import uuid
 
 from strelka import strelka
 
@@ -21,7 +27,33 @@ class ScanStrings(strelka.Scanner):
     def scan(self, data, file, options, expire_at):
         limit = options.get('limit', 0)
 
-        strings = self.strings_regex.findall(data)
-        if limit:
-            strings = strings[:limit]
-        self.event['strings'] = strings
+        #strings = self.strings_regex.findall(data)
+        #if limit:
+        #    strings = strings[:limit]
+        #self.event['strings'] = strings
+        tempfolder = options.get('outdir', '/scanworkdir/')
+        with io.BytesIO(data) as data_stream:
+            temporarylocation=tempfolder + file.uid
+            with open(temporarylocation,'wb') as out: ## Open temporary file as bytes
+                out.write(data_stream.read())         ## Read bytes into file
+
+            ## Do stuff with module/file
+            #flarestrings /data/malware | rank_strings --scores > /data/interesting.txt
+            outfile = "flare-" + uuid.uuid4().hex
+            cmd = "flarestrings " + temporarylocation + " | rank_strings --scores"
+            if limit > 0:
+                cmd = cmd + " --limit " + str(limit) 
+            
+            cmd = cmd + " > " + tempfolder + outfile
+
+            ps = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            output = ps.communicate()[0]
+
+            #if limit:
+            #    output = output[:limit]
+            
+            self.event['strings_outfile'] = outfile #output
+            # print(output)
+            os.remove(temporarylocation) ## Delete file when done
+           
+
