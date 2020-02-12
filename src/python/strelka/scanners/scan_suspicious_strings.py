4e510f242c9a566922b3e12d1a0a281450586c01
Jorge Crisostomo <jorge.crisostomo@chevron.com>
2020-02-11 23:36:31 -0600

    add suspicious strings using yara rules

---
+++ src/python/strelka/scanners/scan_suspicious_strings.py
@@ -0,0 +1,68 @@
+import glob
+import os
+
+import yara
+
+from strelka import strelka
+
+
+class ScanSuspiciousStrings(strelka.Scanner):
+    """Scans files with YARA suspicious strings rules.
+
+    Attributes:
+        compiled_yara: Compiled YARA file derived from YARA rule file(s)
+            in location.
+
+    Options:
+        location: Location of the YARA rules file or directory.
+            Defaults to '/etc/suspicious_rules/'.
+        meta: List of YARA rule meta identifiers
+            (e.g. 'Author') that should be logged.
+            Defaults to empty list.
+    """
+    def init(self):
+        self.compiled_yara = None
+
+    def scan(self, data, file, options, expire_at):
+        location = options.get('location', '/etc/suspicious_rules/')
+        meta = options.get('meta', [])
+
+        try:
+            if self.compiled_yara is None:
+                if os.path.isdir(location):
+                    yara_filepaths = {}
+                    globbed_yara_paths = glob.iglob(f'{location}/**/*.yar*', recursive=True)
+                    for (idx, entry) in enumerate(globbed_yara_paths):
+                        yara_filepaths[f'namespace_{idx}'] = entry
+                    self.compiled_yara = yara.compile(filepaths=yara_filepaths)
+
+                else:
+                    self.compiled_yara = yara.compile(filepath=location)
+
+        except (yara.Error, yara.SyntaxError):
+            self.flags.append('compiling_error')
+
+        self.event['matches'] = []
+        # self.event['meta'] = []
+        self.event['strings'] = []
+
+        # https://yara.readthedocs.io/en/latest/yarapython.html
+        try:
+            if self.compiled_yara is not None:
+                yara_matches = self.compiled_yara.match(data=data)
+                for match in yara_matches:
+                    self.event['matches'].append(match.rule)
+                    self.event['strings'].append(match.strings)
+
+                    #for k, v in match.meta.items():
+                    #    if meta and k not in meta:
+                    #        continue
+
+                        #self.event['meta'].append({
+                        #    'rule': match.rule,
+                        #    'identifier': k,
+                        #    'value': v,
+                        #})
+
+        except (yara.Error, yara.TimeoutError):
+            self.flags.append('scanning_error')
